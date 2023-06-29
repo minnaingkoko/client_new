ae7bc9073c38cca6544c4751ad32495dc477677f
minnaingkoko <minnaingkoko2001@gmail.com>
2023-06-29 16:25:45 +0630

    changes

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,4 +1,4 @@
 <div>
     test
-    <img src='https://shanpyaephyo.s3.ap-south-1.amazonaws.com/airTicket/Aye+Thidar+PP.jpg' alt="">
+    <img src='https://shanpyaephyo.s3.ap-south-1.amazonaws.com/airTicket/Ngu+War+Hlaing+covid.jpg' alt="">
 </div>
